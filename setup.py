502dd0fa83dfff4f7fdb123a4f346e756a0eba88
Piotr Kilczuk <piotr@tymaszweb.pl>
2013-03-27 21:11:36 +0100

    Add setup.py and LICENSE.rst

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+import templatefinder
+
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP',
+]
+
+setup(
+    author='Piotr Kilczuk',
+    author_email='piotr@tymaszweb.pl',
+    name='django-template-finder',
+    version=('.').join(templatefinder.__version__),
+    description='Simple Django utility that allows you to find templates with names matching given pattern',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/TyMaszWeb/django-template-finder',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.2',
+    ],
+    tests_require=[
+    ],
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe = False,
+    #test_suite = 'runtests.main',
+)
--- templatefinder/__init__.py
+++ templatefinder/__init__.py
@@ -1,3 +1,6 @@
 from __future__ import absolute_import
 
 from .utils import *
+
+
+VERSION = (0, 5,)
